23ebce20135f193fa149bc9e7f7f1c753b9bcc24
BMasterton <ICS21-126@camosun.bc.ca>
2024-02-26 10:19:08 -0800

    changed the apps view to the new MainView and not the old ContentView

--- TreasureHunt/MainView.swift
+++ TreasureHunt/MainView.swift
@@ -11,7 +11,7 @@ struct MainView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         //here we are going to do the same thing as in lab one, where depending on what is clicked we will show either the settingsview or the Game view
-        //will need NavigationStack as well as NavigationLink with links to both the settings and details page, with the links being at the bottom 
+        //will need NavigationStack as well as NavigationLink with links to both the settings and details page, with the links being at the bottom
     }
 }
 
