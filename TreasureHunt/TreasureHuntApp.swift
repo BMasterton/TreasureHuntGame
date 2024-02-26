bfcfad084b984d238e55288ee064d422e0f5b10b
BMasterton <ICS21-126@camosun.bc.ca>
2024-02-26 09:46:09 -0800

    Initial Commit

---
+++ TreasureHunt/TreasureHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreasureHuntApp.swift
+//  TreasureHunt
+//
+//  Created by IMD 224 on 2024-02-26.
+//
+
+import SwiftUI
+
+@main
+struct TreasureHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
